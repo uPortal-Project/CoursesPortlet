a555049899ff061612f674d1aedf3d898e2224c1
Jen Bourey <jennifer.bourey@gmail.com>
2012-07-30 12:59:16 -0700

    COURSESPLT-5 Limiting my courses new item count to personal courses portlet

--- courses-portlet-webapp/src/main/java/org/jasig/portlet/courses/mvc/MyCoursesMinimizedStateHandlerInterceptor.java
+++ courses-portlet-webapp/src/main/java/org/jasig/portlet/courses/mvc/MyCoursesMinimizedStateHandlerInterceptor.java
@@ -1,22 +1,3 @@
-/**
- * Licensed to Jasig under one or more contributor license
- * agreements. See the NOTICE file distributed with this work
- * for additional information regarding copyright ownership.
- * Jasig licenses this file to you under the Apache License,
- * Version 2.0 (the "License"); you may not use this file
- * except in compliance with the License. You may obtain a
- * copy of the License at:
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on
- * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied. See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-
 package org.jasig.portlet.courses.mvc;
 
 import javax.portlet.RenderRequest;
@@ -30,7 +11,7 @@ import org.jasig.portlet.courses.model.xml.personal.CoursesByTerm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.portlet.handler.HandlerInterceptorAdapter;
 
-public class MinimizedStateHandlerInterceptor extends HandlerInterceptorAdapter {
+public class MyCoursesMinimizedStateHandlerInterceptor extends HandlerInterceptorAdapter {
 
     @Autowired(required = true)
     private ICoursesDao coursesDao;
@@ -38,6 +19,14 @@ public class MinimizedStateHandlerInterceptor extends HandlerInterceptorAdapter 
     @Override
     public boolean preHandleRender(RenderRequest request, RenderResponse response, Object handler) throws Exception {
         if (WindowState.MINIMIZED.equals(request.getWindowState())) {
+            
+            final TermList termList = coursesDao.getTermList(request);
+            final Term currentTerm = termList.getCurrentTerm();
+            final String termCode = currentTerm.getCode();
+            final CoursesByTerm coursesByTerm = coursesDao.getCoursesByTerm(request, termCode);
+            final int newUpdateCount = coursesByTerm.getNewUpdateCount();
+            response.setProperty("newItemCount", String.valueOf(newUpdateCount));
+            
             return false;
         }
         
