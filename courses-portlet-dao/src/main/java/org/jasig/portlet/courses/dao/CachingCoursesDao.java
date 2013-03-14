00c346c70f00d06ab3109ce2532fe95ef3562413
Eric Dalquist <eric.dalquist@gmail.com>
2013-03-14 15:03:04 -0500

    Add first pass at final grades portlet controller

--- courses-portlet-dao/src/main/java/org/jasig/portlet/courses/dao/CachingCoursesDao.java
+++ courses-portlet-dao/src/main/java/org/jasig/portlet/courses/dao/CachingCoursesDao.java
@@ -11,6 +11,7 @@ import net.sf.ehcache.constructs.blocking.SelfPopulatingCache;
 
 import org.jasig.portlet.courses.model.xml.TermList;
 import org.jasig.portlet.courses.model.xml.personal.CoursesByTerm;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Required;
 
 /**
@@ -18,10 +19,31 @@ import org.springframework.beans.factory.annotation.Required;
  * 
  * @author Eric Dalquist
  */
-public final class CachingCoursesDao implements ICoursesDao {
+public final class CachingCoursesDao implements ICoursesDao, BeanNameAware {
     private ICacheableCoursesDao<Serializable, Serializable> coursesDao;
     private SelfPopulatingCache termListCache;
     private SelfPopulatingCache coursesByTermCache;
+    private boolean scopeKeysToDao = true;
+    private String beanName; 
+    
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+    }
+
+    /**
+     * @param coursesDao The dao to add caching to
+     */
+    public void setEnclosedCoursesDao(ICacheableCoursesDao<Serializable, Serializable> coursesDao) {
+        this.coursesDao = coursesDao;
+    }
+    
+    /**
+     * @param scopeKeysToDao If generated cache key should be scoped to this caching dao instance, defaults to true.
+     */
+    public void setScopeKeysToDao(boolean scopeKeysToDao) {
+        this.scopeKeysToDao = scopeKeysToDao;
+    }
 
     /**
      * @param termListCache The cache to use for term lists
@@ -31,7 +53,8 @@ public final class CachingCoursesDao implements ICoursesDao {
         this.termListCache = new SelfPopulatingCache(termListCache, new CacheEntryFactory() {
             @Override
             public Object createEntry(Object key) throws Exception {
-                return coursesDao.getTermList((Serializable)key);
+                final CacheKey scopedKey = (CacheKey)key;
+                return coursesDao.getTermList(scopedKey.key);
             }
         });
     }
@@ -44,7 +67,8 @@ public final class CachingCoursesDao implements ICoursesDao {
         this.coursesByTermCache = new SelfPopulatingCache(coursesByTermCache, new CacheEntryFactory() {
             @Override
             public Object createEntry(Object key) throws Exception {
-                return coursesDao.getCoursesByTerm((Serializable)key);
+                final CacheKey scopedKey = (CacheKey)key;
+                return coursesDao.getCoursesByTerm(scopedKey.key);
             }
         });
     }
@@ -52,8 +76,9 @@ public final class CachingCoursesDao implements ICoursesDao {
     @Override
     public TermList getTermList(PortletRequest request) {
         final Serializable termListKey = coursesDao.getTermListKey(request);
+        final CacheKey scopedKey = getScopedKey(termListKey);
         
-        final Element element = this.termListCache.get(termListKey);
+        final Element element = this.termListCache.get(scopedKey);
         if (element == null) { 
             return null;
         }
@@ -64,12 +89,75 @@ public final class CachingCoursesDao implements ICoursesDao {
     @Override
     public CoursesByTerm getCoursesByTerm(PortletRequest request, String termCode) {
         final Serializable coursesByTermKey = coursesDao.getCoursesByTermKey(request, termCode);
+        final CacheKey scopedKey = getScopedKey(coursesByTermKey);
 
-        final Element element = this.coursesByTermCache.get(coursesByTermKey);
+        final Element element = this.coursesByTermCache.get(scopedKey);
         if (element == null) { 
             return null;
         }
         
         return (CoursesByTerm)element.getObjectValue();
+    }
+    
+    protected CacheKey getScopedKey(Serializable key) {
+        if (this.scopeKeysToDao) {
+            return new CacheKey(this.beanName, key);
+        }
+        
+        return new CacheKey(key);
+    }
+    
+    private static final class CacheKey implements Serializable {
+        private static final long serialVersionUID = 1L;
+        
+        private final String scope;
+        private final Serializable key;
+        
+        public CacheKey(Serializable key) {
+            this.scope = null;
+            this.key = key;
+        }
+        
+        public CacheKey(String scope, Serializable key) {
+            this.scope = scope;
+            this.key = key;
+        }
+        
+        @Override
+        public int hashCode() {
+            final int prime = 31;
+            int result = 1;
+            result = prime * result + ((key == null) ? 0 : key.hashCode());
+            result = prime * result + ((scope == null) ? 0 : scope.hashCode());
+            return result;
+        }
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+            if (obj == null)
+                return false;
+            if (getClass() != obj.getClass())
+                return false;
+            CacheKey other = (CacheKey) obj;
+            if (key == null) {
+                if (other.key != null)
+                    return false;
+            }
+            else if (!key.equals(other.key))
+                return false;
+            if (scope == null) {
+                if (other.scope != null)
+                    return false;
+            }
+            else if (!scope.equals(other.scope))
+                return false;
+            return true;
+        }
+
+        @Override
+        public String toString() {
+            return "ScopedCacheKey [scope=" + scope + ", key=" + key + "]";
+        }
     }
 }
